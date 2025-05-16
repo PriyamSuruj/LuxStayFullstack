76e217e632a9c9c45f361d5745a9815e8171ecb8
Suruj Priyam Hazarika <116090778+PriyamSuruj@users.noreply.github.com>
2025-05-17 03:28:47 +0530

    stripe done

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import hotelRouter from "./routes/hotelRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import roomRouter from "./routes/roomRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
+import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
 
 connectDB() 
@@ -16,6 +17,9 @@ connectCloudinary()
 
 const app = express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing
+
+// API to listen to Stripe Webhooks
+app.post("/api/stripe",express.raw({ type: "application/json" }),stripeWebhooks);
 
 // Middleware
 app.use(express.json());
