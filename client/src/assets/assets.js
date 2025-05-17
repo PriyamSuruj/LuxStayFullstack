315b78a727c1386668396cf539b77f5783882723
Suruj Priyam Hazarika <116090778+PriyamSuruj@users.noreply.github.com>
2025-05-17 14:48:13 +0530

    hol

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -90,9 +90,9 @@ export const exclusiveOffers = [
 
 // Testimonials Dummy Data
 export const testimonials = [
-    { id: 1, name: "Emma Rodriguez", address: "Barcelona, Spain", image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200", rating: 5, review: "I've used many booking platforms before, but none compare to the personalized experience and attention to detail that LuxStay provides." },
-    { id: 2, name: "Liam Johnson", address: "New York, USA", image: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200", rating: 4, review: "LuxStay exceeded my expectations. The booking process was seamless, and the hotels were absolutely top-notch. Highly recommended!" },
-    { id: 3, name: "Sophia Lee", address: "Seoul, South Korea", image: "https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=200", rating: 5, review: "Amazing service! I always find the best luxury accommodations through LuxStay. Their recommendations never disappoint!" }
+    { id: 1, name: "Anupal Debnath", address: "Barcelona, Spain", image: "https://plus.unsplash.com/premium_vector-1745915292326-63b47202b9bc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29udGFjdCUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D", rating: 5, review: "I've used many booking platforms before, but none compare to the personalized experience and attention to detail that LuxStay provides." },
+    { id: 2, name: "Ksh Sarjoo Leima Singha", address: "New York, USA", image: "https://plus.unsplash.com/premium_vector-1745915292291-f228b4e8e960?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29udGFjdCUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D", rating: 4, review: "LuxStay exceeded my expectations. The booking process was seamless, and the hotels were absolutely top-notch. Highly recommended!" },
+    { id: 3, name: "Aditya Madhab Borah", address: "Seoul, South Korea", image: "https://plus.unsplash.com/premium_vector-1745915292302-3dd54f514e1a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29udGFjdCUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D", rating: 5, review: "Amazing service! I always find the best luxury accommodations through LuxStay. Their recommendations never disappoint!" }
 ];
 
 // Facility Icon
